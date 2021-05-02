c0db0dc7ed84a28dd8f01178433f5ec2b4eb3cfa
Limarceu <60044028+Limarceu@users.noreply.github.com>
2021-05-02 17:45:31 -0300

    starter4

--- audioBook.py
+++ audioBook.py
@@ -6,19 +6,20 @@ import pyttsx3 as tts
 
 
 #To Read Pdf File
-file_path = 'C:\\users\\marcelodiana\\desktop\\Richard Dawkins - O Capelão do Diabo.pdf'
+file_path = 'Richard Dawkins - O Capelão do Diabo.pdf'
 file1 = pdff.PdfFileReader(open(file_path, 'rb'))
 
 #Instance of speaker
 readder = tts.init()
 
 #Reader page by page in Unicode
+#Just because this book, I did start to read from page 6
 for paper in range(6,file1.getNumPages()):
     sheett = file1.getPage(paper)
     ready = sheett.extractText()
     print(ready)
     readder.say(ready)
-    #readder.save_to_file(textPage, 'E:\\book.mp3')
+    #readder.save_to_file(textPage, 'book.mp3')    #If you want save audio
     readder.runAndWait()
 
 
